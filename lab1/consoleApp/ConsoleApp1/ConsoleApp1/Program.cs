4d4067ae321e59434406d3982cf280ddcd2f4167
AvramPop <dany4god@gmail.com>
2020-03-09 08:25:32 -0700

    first lab finished

---
+++ lab1/consoleApp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string connString = "Data Source=DESKTOP-T81R7QV; Initial catalog = Theologians; integrated security=true";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            string query = "select * from Book";
+            SqlCommand command = new SqlCommand(query, conn);
+            SqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                Console.WriteLine("{0} {1} {2} {3}", dataReader[0], dataReader[1], dataReader[2], dataReader[3]);
+            }
+            conn.Close();
+
+            SqlDataAdapter dataAdapterBook = new SqlDataAdapter(query, conn);
+            DataSet dataSet = new DataSet();
+            dataAdapterBook.Fill(dataSet, "Book");
+            foreach(DataRow row in dataSet.Tables["Book"].Rows)
+            {
+                Console.WriteLine("{0} {1} {2} {3}", row["book_id"], row["name"], row["year_published"], row["publisher_id"]);
+            }
+        }
+    }
+}
---
+++ lab1/windowsForms/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+
+        private void FillData() // populate the form with data
+        {
+            //sql connection
+            // sql data adapter, sql data set, data relation
+
+            string connString = "Data Source=DESKTOP-T81R7QV; Initial catalog = Theologians; integrated security=true";
+            conn = new SqlConnection(connString);
+            conn.Open();
+            string publisherSelectQuery = "select * from Publisher";
+
+            SqlDataAdapter dataAdapterPublisher = new SqlDataAdapter(publisherSelectQuery, conn);
+            DataSet dataSet = new DataSet();
+
+            dataAdapterPublisher.Fill(dataSet, "Publisher");
+            dataGridView1.DataSource = dataSet.Tables["Publisher"];
+
+            conn.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            DataSet dataSet = new DataSet();
+            Debug.WriteLine(dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString());
+            string bookSelectQuery = "select * from Book where publisher_id = " + dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+            SqlDataAdapter dataAdapterBook = new SqlDataAdapter(bookSelectQuery, conn);
+            dataAdapterBook.Fill(dataSet, "Book");
+            dataGridView2.DataSource = dataSet.Tables["Book"];
+            conn.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            DataSet dataSet = new DataSet();
+            string addBookQuery = "insert into Book(name, year_published, publisher_id) values ('" +
+                dataGridView2.Rows[dataGridView2.Rows.Count - 2].Cells[1].Value + "', " +
+                dataGridView2.Rows[dataGridView2.Rows.Count - 2].Cells[2].Value + "," +
+                dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString() + ")";
+           SqlCommand addBookCommand = new SqlCommand(addBookQuery, conn);
+           try
+            { 
+            addBookCommand.ExecuteNonQuery();
+        }
+            catch(SqlException ex)
+            {
+                Debug.WriteLine("sql exception");
+            }
+    string bookSelectQuery = "select * from Book where publisher_id = " + dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+
+            SqlDataAdapter dataAdapterBook = new SqlDataAdapter(bookSelectQuery, conn);
+            dataAdapterBook.Fill(dataSet, "Book");
+            dataGridView2.DataSource = dataSet.Tables["Book"];
+            conn.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            DataSet dataSet = new DataSet();
+            string removeBookQuery = "delete from Book where book_id = " + dataGridView2.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+            SqlCommand removeBookCommand = new SqlCommand(removeBookQuery, conn);
+            try
+            { 
+            removeBookCommand.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                Debug.WriteLine("sql exception");
+            }
+    string bookSelectQuery = "select * from Book where publisher_id = " + dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+
+            SqlDataAdapter dataAdapterPublisher = new SqlDataAdapter(bookSelectQuery, conn);
+            dataAdapterPublisher.Fill(dataSet, "Book");
+            dataGridView2.DataSource = dataSet.Tables["Book"];
+            conn.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            DataSet dataSet = new DataSet();
+            string updateBookQuery = "update Book set name ='" +
+                dataGridView2.Rows[dataGridView2.SelectedCells[0].OwningRow.Index].Cells[1].Value + "', year_published = " +
+                dataGridView2.Rows[dataGridView2.SelectedCells[0].OwningRow.Index].Cells[2].Value + ", publisher_id = " + 
+                dataGridView2.Rows[dataGridView2.SelectedCells[0].OwningRow.Index].Cells[3].Value + " where book_id = " + dataGridView2.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+            SqlCommand updatePublisherCommand = new SqlCommand(updateBookQuery, conn);
+            try
+            {
+                updatePublisherCommand.ExecuteNonQuery();
+            }
+            catch(SqlException ex)
+            {
+                Debug.WriteLine("sql exception");
+            }
+            string bookSelectQuery = "select * from Book where publisher_id = " + dataGridView1.SelectedCells[0].OwningRow.Cells[0].Value.ToString();
+
+            SqlDataAdapter dataAdapterPublisher = new SqlDataAdapter(bookSelectQuery, conn);
+            dataAdapterPublisher.Fill(dataSet, "Book");
+            dataGridView2.DataSource = dataSet.Tables["Book"];
+            conn.Close();
+        }
+    }
+}
